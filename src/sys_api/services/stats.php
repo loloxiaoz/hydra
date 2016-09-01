4a7f8d4f5cdd9c38b38513142a7bec49808ad596
左文建 <zuowenjian@mararun.com>
2016-09-01 11:23:48 +0800

    dev env api passed

--- src/sys_api/services/stats.php
+++ src/sys_api/services/stats.php
@@ -60,14 +60,15 @@ class StateSvc extends XRuleService implements XService
     public function events($xcontext,$request,$response)
     {
 
-        $collectorConfs = HydraConf::$queues ;
+        $collectorConfs = HydraConfLoader::getCollectors();
+        $subscribes     = HydraConfLoader::getSubscibes();
         $statsData      = array();
 
         foreach($collectorConfs as $conf )
         {
             list($host,$port) = explode(':',$conf) ;
             $one  = new Pheanstalk_Pheanstalk($host, $port, HydraDefine::TIMEOUT) ;
-            $data = $one->statsTube(HydraConf::TOPIC_EVENT);
+            $data = $one->statsTube(HydraDefine::TOPIC_EVENT);
             $statsData[$conf] = $data ;
         }
         $response->success($statsData);
