c33b7c8af47c79fce56a97365831fbf689c90ba4
左文建 <zuowenjian@mararun.com>
2016-11-14 18:41:56 +0800

    [0.3.1.24]

--- src/sys_dispatch/init.php
+++ src/sys_dispatch/init.php
@@ -2,7 +2,7 @@
 include "pylon/pylon.php" ;
 
 // XSetting::$logMode   = XSetting::LOG_DEBUG_MODE ;
-XSetting::$logMode   = XSetting::LOG_ONLINE_MODE ;
+XSetting::$logMode   = getenv('LOG_MODE') ;
 XSetting::$prjName   = "hydra" ;
 XSetting::$logTag    = XSetting::ensureEnv("USER") ;
 XSetting::$runPath   = XSetting::ensureEnv("RUN_PATH") ;
