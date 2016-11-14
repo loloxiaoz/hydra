1e4afd39e11778b1a2930101f4876809e4ea2a26
左文建 <zuowenjian@mararun.com>
2016-11-14 18:45:13 +0800

    [0.3.1.26]

--- src/sys_dispatch/stat.php
+++ src/sys_dispatch/stat.php
@@ -3,7 +3,7 @@ class  SentryStat
 {
     public function __construct()
     {
-        $DSN="http://a846ab3cb69d47e98ca528211d6e7e13:142174c3e27f4e148b9b4e505e33d26a@sentry.mararun.cn/9";
+        $DSN=getenv('SENTRY_DSN') ;
         $this->client = new Raven_Client($DSN);
     }
     public function stat($name,$data="")
